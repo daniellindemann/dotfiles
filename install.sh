f5d9ac2420a3d22cb0049ca2d3a19c9380b5e848
Daniel Lindemann <d.lindemann@abtis.de>
2022-06-02 09:04:46 +0200

    update to more minimal dotfiles

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+git_mail=$1
+
 echo ""
 echo "Setting up linux dotfiles"
 echo ""
@@ -12,13 +15,19 @@ OUTPUT_LIGHTYELLOW="\033[93m%s\n\033[0m"
 zsh_installed=$(command -v zsh)
 if [[ -z $zsh_installed ]]; then
     printf $OUTPUT_LIGHTYELLOW "You need to install zsh and run this script again"
+else
+    printf $OUTPUT_LIGHTYELLOW "Set zsh as default shell"
+    chsh -s $(which zsh)
 fi
 
 if [[ $zsh_installed ]]; then
-    # chsh -s $(which zsh)
     # install zsh
     wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
     printf $OUTPUT_LIGHTGREEN "oh-my-zsh installed"
+
+    # install powerlevel10k
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    printf $OUTPUT_LIGHTGREEN "powerlevel10k theme installed"
 fi
 
 # get current dir
@@ -33,24 +42,10 @@ ln -sfv "${working_dir}/editorconfig" "${HOME}/.editorconfig" > /dev/null
 ln -sfv "${working_dir}/angular-config.json" "${HOME}/angular-config.json" > /dev/null
 printf $OUTPUT_LIGHTGREEN "Common dotfiles linked"
 
-# setup vs code
-printf $OUTPUT_LIGHTMAGENTA "Setting up Code"
-
-# delete folder structure if already exists
-codefolder_location="${HOME}/.config"
-codefolder_name="Code"
-mkdir -p "${codefolder_location}/${codefolder_name}"
-userfolder="${codefolder_location}/${codefolder_name}/User"
-
-# remove code user folder if it already exists
-if [[ -d "${userfolder}" ]]; then
-    printf $OUTPUT_LIGHTYELLOW "User folder already exists, will remove it"
-    rm -rf "${userfolder}"
+# post configuration
+# set git mail
+if [[ ! -z $git_mail ]]; then
+    git config --global user.email "$git_mail"
 fi
-
-# link code config
-ln -sfv "${working_dir}/Code/User" "${codefolder_location}/${codefolder_name}/User" > /dev/null
-
-printf $OUTPUT_LIGHTGREEN "Code setup completed"
 
 printf $OUTPUT_LIGHTGREEN "dotfiles installed"
